072919d281a9f4faf272bf0249c7c3e41fe6e109
SaeedFSD <saied.fsd@gmail.com>
2021-05-29 18:08:26 +0430

    UI implementation
    Search view, main activity layout and dialogs, detail page

--- app/src/main/java/com/android/movielist/MovieListActivity.java
+++ app/src/main/java/com/android/movielist/MovieListActivity.java
@@ -1,16 +1,35 @@
 package com.android.movielist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.android.movielist.databinding.ActivityMovieListBinding;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MovieListActivity extends AppCompatActivity {
 
 
     private  SearchFilters searchFilters;
+    @BindView(R.id.btn_collapse_filters)
+    public ImageButton btnFiltersCollapse;
+    @BindView(R.id.btn_search)
+    public ImageButton btnSearch;
+    @BindView(R.id.btn_sort)
+    public ImageButton btnSort;
+    @BindView(R.id.btn_genres)
+    public ImageButton btnGenres;
+    @BindView(R.id.filters_parent)
+    public ConstraintLayout filtersParent;
+
+    private boolean filtersIsCollapsed = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +38,23 @@ public class MovieListActivity extends AppCompatActivity {
         setSearchFilterModel(new SearchFilters());
         addFilters();
         activityBinding.setSearchFilters(searchFilters);
-        activityBinding.setClickListener(this);
+        ButterKnife.bind(this);
+        btnFiltersCollapse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (filtersIsCollapsed){
+                    filtersIsCollapsed = false;
+                    btnFiltersCollapse.setImageResource(R.drawable.arrow_up_ico);
+                    filtersParent.setVisibility(View.VISIBLE);
+                }else{
+                    filtersIsCollapsed = true;
+                    btnFiltersCollapse.setImageResource(R.drawable.arrow_down_ico);
+                    filtersParent.setVisibility(View.GONE);
+                }
+            }
+        });
     }
+
 
     private void addFilters() {
         String[] filtersArray = getResources().getStringArray(R.array.search_filters);
